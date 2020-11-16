8f4837c4c14d31542325ea7610c9b5e0c3e809a0
Abhishek Chaturvedi <cabhi83@gmail.com>
2020-11-15 18:35:54 -0800

    Initial Commit

---
+++ force-app/main/default/lwc/objectLookUp/objectLookUp.js
@@ -0,0 +1,81 @@
+import { LightningElement, track, api } from "lwc";
+import findRecords from "@salesforce/apex/PortabilityPolicyService.searchObjects";
+export default class ObjectLookUp extends LightningElement {
+  @track recordsList;
+  searchKey = "";
+  @api selectedValue;
+  @api selectedRecordId;
+  objectApiName;
+  @api iconName;
+  @api lookupLabel;
+  message;
+
+  onLeave(event) {
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    setTimeout(() => {
+      this.searchKey = "";
+      this.recordsList = null;
+    }, 300);
+  }
+
+  onRecordSelection(event) {
+    this.selectedRecordId = event.target.dataset.key;
+    this.selectedValue = event.target.dataset.name;
+    this.searchKey = "";
+    this.onSeletedRecordUpdate();
+  }
+
+  handleKeyChange(event) {
+    const searchKey = event.target.value;
+    this.searchKey = searchKey;
+    this.getLookupResult();
+  }
+ 
+  removeRecordOnLookup(event) {
+    this.searchKey = "";
+    this.selectedValue = null;
+    this.selectedRecordId = null;
+    this.recordsList = null;
+    this.onSeletedRecordUpdate();
+  }
+
+  getLookupResult() {
+    findRecords({
+      searchKey: this.searchKey,
+      objectType: this.objectApiName
+    })
+      .then((result) => {
+        if (result.length === 0) {
+          this.recordsList = [];
+          this.message = "No Records Found";
+        } else {
+          this.recordsList = result;
+          this.message = "";
+        }
+        this.error = undefined;
+      })
+      .catch((error) => {
+        this.error = error;
+        this.recordsList = undefined;
+      });
+  }
+
+  @api
+  get objectName() {
+    return this.objectApiName;
+  }
+
+  set objectName(name) {
+    this.objectApiName = name;
+    this.removeRecordOnLookup();
+  }
+  onSeletedRecordUpdate() {
+    const passEventr = new CustomEvent("recordselection", {
+      detail: {
+        selectedRecordId: this.selectedRecordId,
+        selectedValue: this.selectedValue
+      }
+    });
+    this.dispatchEvent(passEventr);
+  }
+}
---
+++ force-app/main/default/lwc/portabilityRequestForm/portabilityRequestForm.js
@@ -0,0 +1,113 @@
+import { LightningElement, wire, api, track } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import getPolicyDetails from "@salesforce/apex/PortabilityPolicyService.getPolicyDetails";
+import createDSR from "@salesforce/apex/PortabilityPolicyService.createDSR";
+
+const OBJECT_OPTIONS = [
+  { label: "Account", value: "Account" },
+  { label: "Contact", value: "Contact" },
+  { label: "User", value: "User" },
+  { label: "Lead", value: "Lead" },
+  { label: "Individual", value: "Individual" }
+];
+const ICONS = {
+  Account: "standard:account",
+  Lead: "standard:lead",
+  Contact: "standard:contact",
+  User: "standard:user",
+  Individual: "standard:individual"
+};
+const DSR_SUCCESS_TITLE = "Success! DSR Request Sent";
+const DSR_SUCCESS_MESSAGE =
+  "Check the DSR Record in some time to check file generation status.";
+const APEX_ERROR_MESSAGE =
+  "There was a problem processing your request. Please try again.";
+const APEX_ERROR_TITLE = "Error: Request Not Sent";
+const VARIANT_OPTIONS = {
+  error: "error",
+  success: "success"
+};
+
+export default class PorabilityRequestForm extends LightningElement {
+  selectedPolicyName;
+  selectedObjectType;
+  selectedRecordId;
+  selectedRecordName;
+
+  selectedIcon;
+  error;
+  @track policyOptions;
+
+  @wire(getPolicyDetails)
+  wiredPolicies({ data, error }) {
+    if (data) {
+      this.policyOptions = data.map((policy) => {
+        return { label: policy.DeveloperName, value: policy.DeveloperName };
+      });
+      console.log(this.policyOptions);
+    } else if (error) {
+      this.policyOptions = undefined;
+      this.error = error;
+      console.log(error.body);
+    }
+  }
+
+  get objectOptions() {
+    return OBJECT_OPTIONS;
+  }
+  handlePolicyOptionChange(event) {
+    this.selectedPolicyName = event.target.value;
+    console.log(this.selectedPolicyName);
+  }
+
+  handleObjectTypeChange(event) {
+    this.selectedObjectType = event.target.value;
+    this.selectedIcon = ICONS[this.selectedObjectType];
+    console.log(this.selectedObjectType);
+  }
+
+  onRecordSelection(event) {
+    this.selectedRecordName = event.detail.selectedValue;
+    this.selectedRecordId = event.detail.selectedRecordId;
+  }
+
+  sendRequest() {
+    createDSR({
+      dataSubjectId: this.selectedRecordId,
+      policyName: this.selectedPolicyName
+    })
+      .then(() => {
+        this.error = undefined;
+        this.selectedRecordId = undefined;
+        this.selectedRecordName = undefined;
+        this.querySelector();
+        const evt = new ShowToastEvent({
+          title: DSR_SUCCESS_TITLE,
+          message: DSR_SUCCESS_MESSAGE,
+          variant: VARIANT_OPTIONS.success
+        });
+        this.dispatchEvent(evt);
+        this.resetComponent();
+      })
+      .catch((error) => {
+        this.error = error;
+        this.selectedRecordId = undefined;
+        this.selectedRecordName = undefined;
+        const evt = new ShowToastEvent({
+          title: APEX_ERROR_TITLE,
+          message: APEX_ERROR_MESSAGE,
+          variant: VARIANT_OPTIONS.error
+        });
+        this.dispatchEvent(evt);
+        this.resetComponent();
+      });
+  }
+
+  resetComponent() {
+    this.selectedPolicyName = null;
+    this.selectedObjectType = null;
+    this.selectedRecordId = null;
+    this.selectedRecordName = null;
+    this.selectedIcon = null;
+  }
+}
