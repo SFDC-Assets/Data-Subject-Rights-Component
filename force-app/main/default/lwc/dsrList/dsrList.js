a0f7864ea6bf45067cf3134d9f9723685bbb0a95
Abhishek Chaturvedi <cabhi83@gmail.com>
2020-11-16 19:02:25 -0800

    Everything working. Update status pending

--- force-app/main/default/lwc/dsrList/dsrList.js
+++ force-app/main/default/lwc/dsrList/dsrList.js
@@ -1,25 +1,33 @@
-import { LightningElement, wire, track } from "lwc";
+import { LightningElement, wire } from "lwc";
 import getAllDSRs from "@salesforce/apex/PortabilityPolicyService.getAllDSRs";
+import deleteAllDSRs from "@salesforce/apex/PortabilityPolicyService.deleteAllDSRs";
+import deleteDSR from "@salesforce/apex/PortabilityPolicyService.deleteDSR";
 import DSRMC from "@salesforce/messageChannel/DSRMessageChannel__c";
 import { refreshApex } from "@salesforce/apex";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+
 import {
   subscribe,
   MessageContext,
   APPLICATION_SCOPE
 } from "lightning/messageService";
+const VARIANT_OPTIONS = {
+  error: "error",
+  success: "success"
+};
 const COLS = [
   { label: "Request #", fieldName: "Name", editable: false },
   {
     label: "Subject Name",
     fieldName: "Data_Subject_Name__c",
     editable: false,
-    sortable: true
+    sortable: false
   },
   {
     label: " Subject Type",
     fieldName: "Data_Subject_Type__c",
     editable: false,
-    sortable: true
+    sortable: false
   },
   { label: "Status", fieldName: "Status__c", editable: false, sortable: true },
 
@@ -27,59 +35,98 @@ const COLS = [
     label: "Policy Name",
     fieldName: "Policy_Name__c",
     editable: false,
-    sortable: true
+    sortable: false
   },
   {
     label: "Policy File URL",
     fieldName: "Policy_File_URL__c",
     type: "url",
     editable: false,
-    sortable: true
+    sortable: false
+  },
+  {
+    type: "action",
+    typeAttributes: {
+      rowActions: [{ label: "Delete", name: "delete" }],
+      menuAlignment: "auto"
+    }
   },
   {
     type: "button",
     typeAttributes: {
       iconName: "action:refresh",
-      name: "View",
-      title: "View",
+      name: "refresh",
+      title: "refresh",
       disabled: false,
-      value: "view",
-      iconPosition: "left"
+      value: "refresh",
+      iconPosition: "center"
     }
   }
 ];
 export default class DsrList extends LightningElement {
-  requests;
+  //requests;
   error;
 
+  isLoading = false;
   recordId;
+
+  hasRecords = false;
   get columns() {
     return COLS;
   }
-
+  get recordsAvailable() {
+    console.log(this.requests);
+    return this.hasRecords;
+  }
   // wired message context
   @wire(MessageContext)
   messageContext;
 
-  @track requests;
-
-  refresh() {
-    getAllDSRs()
-      .then((result) => {
-        this.requests = result;
-        return this.requests;
-      })
-      .catch((error) => {
-        this.error = error;
+  @wire(getAllDSRs)
+  wiredRequests(result) {
+    this.isLoading = true;
+    this.requests = result;
+    if (this.requests.data) {
+      if (this.requests.data.length > 0) {
+        this.hasRecords = true;
+      }
+      this.isLoading = false;
+    } else if (this.requests.error) {
+      this.hasRecords = false;
+      this.isLoading = false;
+      const evt = new ShowToastEvent({
+        title: "Error Loading Data",
+        message: "Please check console for error message",
+        variant: VARIANT_OPTIONS.error
       });
+      this.dispatchEvent(evt);
+    }
+  }
+
+  subscription;
+
+  async refresh() {
+    this.isLoading = true;
+    try {
+      await refreshApex(this.requests);
+      this.isLoading = false;
+    } catch (error) {
+      this.error = error;
+      this.isLoading = false;
+      const evt = new ShowToastEvent({
+        title: "Error Loading Data",
+        message: "Please check console for error message",
+        variant: VARIANT_OPTIONS.error
+      });
+      this.dispatchEvent(evt);
+    }
   }
   subscribeMC() {
-    console.log("Inside Subscription");
     this.subscription = subscribe(
       this.messageContext,
       DSRMC,
       (message) => {
-        console.log("Received Message" + message.toString());
+        this.isLoading = true;
         this.refresh(message);
       },
       { scope: APPLICATION_SCOPE }
@@ -87,20 +134,70 @@ export default class DsrList extends LightningElement {
   }
   // Runs when component is connected, subscribes to BoatMC
   connectedCallback() {
-    // recordId is populated on Record Pages, and this component
-    // should not update when this component is on a record page.
+    this.isLoading = true;
     if (this.subscription || this.recordId) {
       return;
     }
-    // Subscribe to the message channel to retrieve the recordID and assign it to boatId.
     this.subscribeMC();
+  }
 
-    getAllDSRs()
-      .then((result) => {
-        this.requests = result;
+  refreshRecord(event) {
+    const recId = event.detail.row.Id;
+    const actionName = event.detail.action.name;
+    if (actionName === "refresh") {
+      // eslint-disable-next-line no-alert
+      alert(recId);
+    } else if (actionName === "delete") {
+      deleteDSR({ recordId: recId })
+        .then(() => {
+          this.refresh();
+          this.isLoading = false;
+          this.hasRecords = false;
+          const evt = new ShowToastEvent({
+            title: "Success",
+            message: "Request Deleted!",
+            variant: VARIANT_OPTIONS.success
+          });
+          this.dispatchEvent(evt);
+        })
+        .catch((error) => {
+          this.error = error;
+          this.isLoading = false;
+          console.log(this.error);
+          const evt = new ShowToastEvent({
+            title: "Error in Processing",
+            message: "Please check console for error message",
+            variant: VARIANT_OPTIONS.error
+          });
+          this.dispatchEvent(evt);
+        });
+    }
+  }
+
+  deleteAll() {
+    this.isLoading = true;
+    deleteAllDSRs()
+      .then(() => {
+        this.refresh();
+        this.isLoading = false;
+        this.hasRecords = false;
+        const evt = new ShowToastEvent({
+          title: "Success",
+          message: "All Requests deleted!",
+          variant: VARIANT_OPTIONS.success
+        });
+        this.dispatchEvent(evt);
       })
       .catch((error) => {
         this.error = error;
+        this.isLoading = false;
+        console.log(this.error);
+        const evt = new ShowToastEvent({
+          title: "Error in Processing",
+          message: "Please check console for error message",
+          variant: VARIANT_OPTIONS.error
+        });
+        this.dispatchEvent(evt);
       });
   }
 }
--- force-app/main/default/lwc/portabilityRequestForm/portabilityRequestForm.js
+++ force-app/main/default/lwc/portabilityRequestForm/portabilityRequestForm.js
@@ -4,6 +4,8 @@ import { MessageContext, publish } from "lightning/messageService";
 import getPolicyDetails from "@salesforce/apex/PortabilityPolicyService.getPolicyDetails";
 import createDSR from "@salesforce/apex/PortabilityPolicyService.createDSR";
 import DSRMC from "@salesforce/messageChannel/DSRMessageChannel__c";
+import { subscribe as empSubscribe, onError } from "lightning/empApi";
+
 const OBJECT_OPTIONS = [
   { label: "Account", value: "Account" },
   { label: "Contact", value: "Contact" },
@@ -24,12 +26,17 @@ const DSR_SUCCESS_MESSAGE =
 const APEX_ERROR_MESSAGE =
   "There was a problem processing your request. Please try again.";
 const APEX_ERROR_TITLE = "Error: Request Not Sent";
+const EVENT_ERROR_MESSAGE =
+  "Please manually refresh page to see updated status.";
+const EVENT_ERROR_TITLE = "Error Refreshing Data";
 const VARIANT_OPTIONS = {
   error: "error",
   success: "success"
 };
 
 export default class PorabilityRequestForm extends LightningElement {
+  empSubscription;
+  channelName = "/event/Data_Subject_Request_Event__e";
   selectedPolicyName;
   selectedObjectType;
   selectedRecordId;
@@ -80,12 +87,11 @@ export default class PorabilityRequestForm extends LightningElement {
       subjectName: this.selectedRecordName
     })
       .then((result) => {
-        console.log(result);
         this.error = undefined;
         this.selectedRecordId = undefined;
         this.selectedRecordName = undefined;
         this.querySelector();
-        publish(this.messageContext, DSRMC, { msg: "New DSR Created" });
+        publish(this.messageContext, DSRMC, { msg: result });
         const evt = new ShowToastEvent({
           title: DSR_SUCCESS_TITLE,
           message: DSR_SUCCESS_MESSAGE,
@@ -96,6 +102,7 @@ export default class PorabilityRequestForm extends LightningElement {
       })
       .catch((error) => {
         this.error = error;
+        console.log(this.error);
         this.selectedRecordId = undefined;
         this.selectedRecordName = undefined;
         const evt = new ShowToastEvent({
@@ -114,5 +121,49 @@ export default class PorabilityRequestForm extends LightningElement {
     this.selectedRecordId = null;
     this.selectedRecordName = null;
     this.selectedIcon = null;
+  }
+
+  connectedCallback() {
+    if (this.empSubscription) {
+      return;
+    }
+    this.handleSubscribe();
+    this.registerErrorListener();
+  }
+
+  handleSubscribe() {
+    // Callback invoked whenever a new event message is received
+    const messageCallback = (response) => {
+      publish(this.messageContext, DSRMC, {
+        msg: JSON.stringify(response.data.payload.DSR_ID__c)
+      });
+    };
+
+    // Invoke subscribe method of empApi. Pass reference to messageCallback
+    empSubscribe(this.channelName, -1, messageCallback).then((response) => {
+      // Response contains the subscription information on subscribe call
+      console.log(
+        "Subscription request sent to: ",
+        JSON.stringify(response.channel)
+      );
+      this.empSubscription = response;
+    });
+  }
+
+  registerErrorListener() {
+    // Invoke onError empApi method
+    onError((error) => {
+      this.error = error;
+      console.log("Received error from server: ", JSON.stringify(error));
+      this.selectedRecordId = undefined;
+      this.selectedRecordName = undefined;
+      const evt = new ShowToastEvent({
+        title: EVENT_ERROR_TITLE,
+        message: EVENT_ERROR_MESSAGE,
+        variant: VARIANT_OPTIONS.error
+      });
+      this.dispatchEvent(evt);
+      this.resetComponent();
+    });
   }
 }
